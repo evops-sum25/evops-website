297fff7152f650f308fd4a2e842ffa4cf11679ae
Projacktor <1234xl.com@gmail.com>
2025-06-22 23:35:21 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main>
-      <h1></h1>
+    <main className="w-screen h-screen flex flex-col p-8 items-center">
+
+      <h1 className="text-4xl text-black font-bold">Some header text here</h1>
     </main>
   );
 }

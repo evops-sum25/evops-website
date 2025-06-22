aa3f40a1f46efa50ccf820a7c107344d21f43749
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-23 02:59:41 +0300

    .

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -4,29 +4,18 @@ import Image from "next/image";
 
 export default function EventFeed() {
   const tagData = [
-    { name: "study", color: "orange" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
-    { name: "study", color: "green" },
+    { name: "study", color: "blue" },
+    { name: "bdsm", color: "red" },
+    { name: "b23", color: "yellow" },
+    { name: "official", color: "green" },
+    { name: "games", color: "purple" },
+    { name: "food", color: "orange" },
   ];
 
   return (
     <main className="flex w-full max-w-full flex-col">
       <Image src={test} alt="event" className="w-full" />
-      <div className="flex flex-row gap-2 overflow-auto p-2">
+      <div className="flex flex-row flex-wrap gap-2 p-2">
         {tagData.map(({ name, color }, i) => (
           <Tag key={i} name={name} color={color} />
         ))}

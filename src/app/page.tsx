75dea7f13a50b996ed800f8be0a375003aa475a4
Projacktor <1234xl.com@gmail.com>
2025-06-22 23:52:55 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
-  return (
-    <main className="w-screen h-screen flex flex-col p-8 items-center">
+import test from "../../public/test.png";
+import Image from "next/image";
 
-      <h1 className="text-4xl text-black font-bold">Some header text here</h1>
+export default function Home(): React.ReactNode {
+  return (
+    <main className="w-screen h-screen flex flex-col items-center">
+      <div className="flex flex-col items-center border-2 w-full border-accent">
+        <h1 className="text-3xl text-black font-bold w-full pl-4">Probstat Final Preparation</h1>
+        <Image src={test} alt="event"/>
+      </div>
     </main>
   );
 }

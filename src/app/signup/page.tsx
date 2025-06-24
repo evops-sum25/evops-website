25a1da928cf28b3a72651a53c29dc51222b0da78
Asqar Arslanov <asqarslanov@gmail.com>
2025-06-24 15:44:18 +0300

    better sign up form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,28 +1,27 @@
-import { Link, User } from "lucide-react";
+import { ChevronLeft, User } from "lucide-react";
 
 export default function SignUpPage() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center">
-      <fieldset className="fieldset flex w-128 max-w-full flex-col gap-4">
-        <legend className="fieldset-legend">New account</legend>
+    <>
+      <nav>
+        <a href="../" className="btn btn-ghost btn-circle">
+          <ChevronLeft />
+        </a>
+      </nav>
+      <main className="flex h-screen w-screen flex-col items-center justify-center p-4">
+        <fieldset className="fieldset flex w-full max-w-96 flex-col items-center gap-4">
+          <legend className="fieldset-legend w-full">
+            <h1 className="w-full text-center text-lg">New Account</h1>
+          </legend>
 
-        <div className="input w-full">
-          <User className="text-base-content/50 size-4" />
-          <input type="text" className="w-full" required placeholder="Name" />
-        </div>
+          <div className="input w-full">
+            <User className="text-base-content/50 size-4" />
+            <input type="text" className="w-full" required placeholder="Name" />
+          </div>
 
-        <div className="input w-full">
-          <Link className="text-base-content/50 size-4" />
-          <input
-            type="url"
-            required
-            className="w-full"
-            placeholder="Profile picture URL"
-          />
-        </div>
-
-        <button className="btn btn-primary">Sign up</button>
-      </fieldset>
-    </main>
+          <button className="btn btn-primary w-full">Sign up</button>
+        </fieldset>
+      </main>
+    </>
   );
 }

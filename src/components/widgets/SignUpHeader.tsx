da98f7190bb874fbb3b30bdcd2ff3b10ad57b5e6
Projacktor <1234xl.com@gmail.com>
2025-07-02 00:12:08 +0300

    feat: add i18n to header

--- src/components/widgets/SignUpHeader.tsx
+++ src/components/widgets/SignUpHeader.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useT } from "@/app/i18n/client";
 
 export default function SignUpHeader() {
+  const { t } = useT("common", { keyPrefix: "signUpHeader" });
   const router = useRouter();
   return (
-    <header className="flex w-full flex-row items-center justify-between p-3">
-      <p className="text-base-content font-semibold">Hi, guest!</p>
+    <header className="flex w-full flex-row items-center justify-between py-3 px-23">
+      <p className="text-base-content font-semibold">{t("greeting")}</p>
       <button
         className="btn btn-primary"
         onClick={() => router.push("/signup")}
       >
-        Sign Up
+        {t('button')}
       </button>
     </header>
   );

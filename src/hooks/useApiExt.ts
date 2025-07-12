a5a83f2d891978b6d0b29a0431d37826b285fdd8
Asqar Arslanov <asqarslanov@gmail.com>
2025-07-12 23:51:43 +0300

    feat: support api-ext with extism

---
+++ src/hooks/useApiExt.ts
@@ -0,0 +1,14 @@
+import { type ApiExt, initApiExt } from '@/lib/api/ext'
+import { useEffect, useState } from 'react'
+
+export default function useApiExt() {
+  const [apiExt, setApiExt] = useState<ApiExt | undefined>(undefined)
+  useEffect(() => {
+    async function loadApiExt() {
+      const binary = await fetch('/evops.wasm').then((r) => r.arrayBuffer())
+      setApiExt(await initApiExt(binary))
+    }
+    loadApiExt()
+  })
+  return apiExt
+}
